f4850ee27e2c5219ab90e7f300762105879fe363
bluepebble25 <bluepebble25@gmail.com>
2023-08-03 20:47:57 +0900

    feat: 차트의 격자 제거 및 별점 평균 계산하는 로직 추가 (barChart)

--- barChart/index.js
+++ barChart/index.js
@@ -6,6 +6,17 @@ const ratings = [
   { stars: 5, count: 520 },
 ];
 
+/* 별점 평균 표시하는 부분 */
+const totalStars = ratings.reduce(
+  (acc, curr) => acc + curr.stars * curr.count,
+  0
+);
+const totalCount = ratings.reduce((acc, curr) => acc + curr.count, 0);
+const average = (totalStars / totalCount).toFixed(1);
+const averageElement = document.getElementById('average');
+averageElement.innerHTML = `평균 ${average}점`;
+
+/* chart 표시하는 부분 */
 const ctx = document.getElementById('myChart');
 
 new Chart(ctx, {
@@ -22,10 +33,18 @@ new Chart(ctx, {
   },
   options: {
     scales: {
-      maintainAspectRatio: false,
+      x: {
+        grid: {
+          display: false, // bar 구분하는 격자 숨김
+        },
+      },
       y: {
         beginAtZero: true,
+        grid: {
+          display: false, // 눈금 격자 숨김
+        },
         ticks: {
+          display: false,
           callback: function (value, index, values) {
             return value.toLocaleString();
           },
